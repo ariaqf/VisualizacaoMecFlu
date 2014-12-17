bd3ec2edaac85ef0f98d41eeb9c4fb6574d27f4d
gabrielsqsf <gabrielsqsf@gmail.com>
2014-12-17 19:27:32 -0200

    alteracoes, ainda falta acertar o algoritmo

--- cell.h
+++ cell.h
@@ -1,15 +1,13 @@
 #ifndef _INCLUDE_CELL
 #define _INCLUDE_CELL
 
-#include <vector>
+#include <iostream>
 
-using std;
-
-class Liquid 
+/*class Liquid 
 {
 	private double density, kinematic_viscosity;
 	public Liquid();
-};
+};*/
 
 /*class Particle
 {
@@ -32,20 +30,41 @@ class Liquid
 
 class Cell 
 {
-	private double x, y, width, height, speed_x, speed_y;
-	private bool isVoid, hasLiquid, isBorder;
-	public Cell();
-	public Cell(double x_, double y_, double w_, double h_);
-	public void setSpeed(double x_, double y_) 
-	{
-		speed_x = x_;
-		speed_y = y_;
-	}
-	public void addSpeed(double x_, double y_) 
-	{
-		speed_x += x_;
-		speed_y += y_;
-	}
+	private:
+	 double width, height;
+	public:
+	 bool isVoid, hasLiquid, isBorder;
+	 double speed_x, speed_y, leftSpeed, rightSpeed, topSpeed, bottomSpeed;
+	 Cell() {
+	 	height = 10;
+	 	width = 10;
+	 	isVoid = false;
+	 	hasLiquid = false;
+	 	isBorder = false;
+	 	speed_x = 0;
+	 	speed_y = 0;
+	 	leftSpeed = 0;
+	 	rightSpeed = 0;
+	 	topSpeed = 0;
+	 	bottomSpeed = 0;
+	 }
+	 Cell(double w_, double h_) {
+	 	height = h_;
+	 	width = w_;
+	 	isVoid = false;
+	 	hasLiquid = false;
+	 	isBorder = false;
+	 	speed_x = 0;
+	 	speed_y = 0;
+	 	leftSpeed = 0;
+	 	rightSpeed = 0;
+	 	topSpeed = 0;
+	 	bottomSpeed = 0;
+	 }
+	 void updateSpeed() {
+	 	speed_x = (leftSpeed+rightSpeed)/2;
+	 	speed_y = (topSpeed+bottomSpeed)/2;
+	 }
 };
 
 #endif
